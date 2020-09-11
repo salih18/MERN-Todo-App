8069f317dc9e7f64d6bc2c21fcc17bc94574a530
salih <salihsert18@gmail.com>
2020-09-11 17:28:08 +0200

    prepare for heroku deployment

--- server.js
+++ server.js
@@ -19,13 +19,14 @@ app.use('/api/tags', require('./routes/api/tags'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-} 
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
